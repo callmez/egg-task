c63bd92d728ddf07bcc3e962353057848777d5b6
CallMeZ <callme-z@qq.com>
2018-06-20 20:07:54 +0800

    no message

--- class/Task.js
+++ class/Task.js
@@ -9,18 +9,58 @@ module.exports = class Task extends BaseContextClass {
     this.task = ctx.task;
   }
 
-  add(opt = {}) {
+  /**
+   * 通过复写该方法获得该task默认选项
+   * @return {{}}
+   */
+  get options() {
+    return {};
+  }
+
+  /**
+   * 添加任务. 默认执行该操作,可复写更改任务流程
+   * @param data
+   * @param options
+   * @return {*}
+   */
+  add(data, options = {}) {
+    return this.addTask(data, {
+      ...this.options,
+      ...options,
+    });
+  }
+
+  /**
+   * 执行任务. 复写该方法执行任务流程
+   * @return {Promise<*>}
+   */
+  async process(job) {
+    return this.app.logger.error('process method must be override');
+  }
+
+  /**
+   * 该方法为基础调用. 请勿复写
+   * @param data
+   * @param options
+   * @return {*}
+   */
+  addTask(data, options) {
     const { fullPath, pathName } = this;
-    this.queue.add({
+    return this.queue.add({
       meta: {
         fullPath,
         pathName,
       },
-      ...opt,
-    });
+      ...data,
+    }, options);
   }
 
-  process(task = {}) {
-
+  /**
+   * 该方法为基础调用. 请勿复写
+   * @param job
+   * @return {Promise<*>}
+   */
+  async processTask(job) {
+    return this.process(job);
   }
 };
